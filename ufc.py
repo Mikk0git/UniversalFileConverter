2ca16c16a42dc2a06fbadb287331677995b1b299
mikk0git <you@example.com>
2023-07-07 16:29:16 +0200

    Window config

--- ufc.py
+++ ufc.py
@@ -4,7 +4,10 @@ from tkinter import ttk
 class App():
     def __init__(self):
         self.root = tk.Tk()
-
+        self.root.geometry("600x450")
+        self.root.title("UniversalFileConverter")
+        self.mainframe = tk.Frame(self.root, background="gray")
+        self.mainframe.pack(fill='both',expand=True)
         self.root.mainloop()
         return
     
